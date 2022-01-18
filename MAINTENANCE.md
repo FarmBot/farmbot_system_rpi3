e380da6f9661b8e3c29ee0ba3b595937f452a82c
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2022-01-18 13:41:29 -0800

    merge v1.18.1 upstream

--- MAINTENANCE.md
+++ MAINTENANCE.md
@@ -1,8 +1,8 @@
 # Maintaining This Repo
 
 ```bash
-git clone git@github.com:Farmbot-Labs/nerves_system_farmbot_rpi3.git
-cd nerves_system_farmbot_rpi3
+git clone git@github.com:Farmbot/farmbot_system_rpi3.git
+cd farmbot_system_rpi3
 git remote add upstream git@github.com:Nerves-Project/nerves_system_rpi3.git
 git fetch --all
 git merge v<new upstream release>
@@ -12,6 +12,6 @@ git clone git@github.com:Nerves-Project/nerves_system_br -b v<latest release>
 make system # this will take a while.
 # create github release.
 # upload system artifact.
-cd nerves_system_farmbot_rpi3
+cd farmbot_system_rpi3
 mix hex.publish package
 ```
--- mix.exs
+++ mix.exs
@@ -64,8 +64,8 @@ defmodule FarmbotSystemRpi3.Mixfile do
   defp deps do
     [
       {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.4", runtime: false},
-      {:nerves_system_br, "1.17.4", runtime: false},
-      {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.4.3", runtime: false},
+      {:nerves_system_br, "1.18.3", runtime: false},
+      {:nerves_toolchain_armv7_nerves_linux_gnueabihf, "~> 1.5.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
     ]
@@ -90,7 +90,7 @@ defmodule FarmbotSystemRpi3.Mixfile do
   defp package do
     [
       files: package_files(),
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => @source_url}
     ]
   end
